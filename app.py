9d1d5efb08b1ccdf6d35630a9606d8bd08da0c96
Sandro Brito <brittosandro@gmail.com>
2023-02-26 00:51:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
+import sqlite3
 import streamlit as st
 import re
-import smtplib
-import pandas as pd
-import base64
-import io
-import csv
-
-
-def salvar_dados(nome, sobrenome, matricula, email, serie, turma, curso):
-    novo_registro = [nome, sobrenome, matricula, email, serie, turma, curso]
-    with open('dados.csv', 'a') as arquivo:
-        writer = csv.writer(arquivo)
-        writer.writerow(novo_registro)
-
-
-# Função para enviar email com os dados preenchidos
-def enviar_email(email, nome, sobrenome, matricula, serie, turma, curso):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login("nome@gmail.com", "senha")
-    msg = f"Olá {nome} {sobrenome},\n\nSegue abaixo os dados preenchidos no formulário:\n\nNome: {nome}\nSobrenome: {sobrenome}\nMatrícula: {matricula}\nSérie: {serie}\nTurma: {turma}\nCurso selecionado: {curso}\n\nObrigado por se inscrever no Colégio Estadual Cora Coralina."
-    server.sendmail("seu_email@gmail.com", email, msg)
-    server.quit()
 
 # Função para validar o formato do email
 def validar_email(email):
@@ -30,16 +9,15 @@ def validar_email(email):
         return False
     return True
 
-# Criação de um dataframe para armazenar os dados
-df = pd.DataFrame(columns=['Nome', 'Sobrenome', 'Matrícula', 'E-mail', 'Série', 'Turma', 'Curso'])
+# Cria uma conexão com o banco de dados
+conn = sqlite3.connect('usuarios.db')
 
-col1, col2 = st.columns([1, 0.4,])
-with col1:
-    st.title('Formulário de Cadastro')
-    st.subheader('Colégio Estadual Cora Coralina')
-with col2:
-    st.image('logo_cora.png', width=60)
+# Cria uma tabela para armazenar os dados do usuário
+conn.execute('''CREATE TABLE IF NOT EXISTS usuarios
+                (nome TEXT, sobrenome TEXT, matricula TEXT, email TEXT, serie TEXT, turma TEXT, curso TEXT)''')
 
+# Título do aplicativo
+st.title('Formulário de Inscrição')
 
 # Campos para entrada de dados
 with st.form('my_form'):
@@ -49,45 +27,22 @@ with st.form('my_form'):
         sobrenome = st.text_input('Sobrenome')
         matricula = st.text_input('Matrícula')
         email = st.text_input('E-mail')
+    with col2:
         serie = st.text_input('Série')
         turma = st.text_input('Turma')
-    with col2:
         st.subheader('Curso')
-        cursos = ['Auxiliar Financeiro', 'Auxiliar Administrativo',
-                  'Auxiliar Agroecologia', 'Auxiliar Agropecuária',
-                  'Assistente de Logística', 'Vendedor']
+        cursos = ['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4', 'Curso 5', 'Curso 6']
         curso_selecionado = st.radio('Selecione um curso:', cursos)
     # Botão de envio
     submit_button = st.form_submit_button(label='Enviar')
 
-
-# Impressão das informações inseridas pelo usuário e armazenamento no dataframe
+# Insere os dados do usuário no banco de dados
 if submit_button:
     if validar_email(email):
-        st.write('Nome:', nome)
-        st.write('Sobrenome:', sobrenome)
-        st.write('Matrícula:', matricula)
-        st.write('E-mail:', email)
-        st.write('Série:', serie)
-        st.write('Turma:', turma)
-        st.write('Curso selecionado:', curso_selecionado)
+        conn.execute("INSERT INTO usuarios VALUES (?, ?, ?, ?, ?, ?, ?)",
+                    (nome, sobrenome, matricula, email, serie, turma, curso_selecionado))
+        conn.commit()
+        st.success('Dados enviados com sucesso!')
 
-        #enviar_email(email, nome, sobrenome, matricula, serie, turma, curso_selecionado)
-
-        # Adiciona as informações no dataframe
-        novo_registro = {'Nome': nome, 'Sobrenome': sobrenome, 'Matrícula': matricula, 'E-mail': email, 'Série': serie, 'Turma': turma, 'Curso': curso_selecionado}
-        df = df.append(novo_registro, ignore_index=True)
-
-        # Salva os dados do DataFrame em um arquivo Excel
-        df.to_excel('dados.xlsx', index=False)
-        
-        salvar_dados(nome, sobrenome, matricula, email, serie, turma, curso_selecionado)
-        
-        # Adiciona botão para download da tabela
-        # Adiciona botão para download da tabela
-        with io.BytesIO() as buffer:
-            with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
-                df.to_excel(writer, index=False)
-            b64 = base64.b64encode(buffer.getvalue()).decode()
-        download_button_str = f'<a href="data:application/octet-stream;base64,{b64}" download="dados.xlsx">Download Tabela</a>'
-        st.markdown(download_button_str, unsafe_allow_html=True)
+# Fecha a conexão com o banco de dados
+conn.close()
